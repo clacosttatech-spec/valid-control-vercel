511b264b7d325adea76f531b3d5a1b5dda4a7cc2
clacosttatech-spec <clacosttatech@gmail.com>
2025-07-31 18:02:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // 🔧 ESSENCIAL para funcionar no Netlify
   plugins: [react()],
   build: {
     outDir: 'dist'
